35ade1c17012a0720fad34516c37636512689789
ValeriaBassaniVDM <valeria.bassani@vidiemme.it>
2024-07-02 17:54:20 +0200

    error(fail)

--- src/components/SignUpLogiInForm/SignUpForm.tsx
+++ src/components/SignUpLogiInForm/SignUpForm.tsx
@@ -1,4 +1,4 @@
-import { useReducer } from "react";
+import { useReducer, useState } from "react";
 import './SignUpForm.css';
 import { Link } from "react-router-dom";
 
@@ -15,13 +15,21 @@ type State = {
     mail: string;
     password: string;
     confirmPassword: string;
+    // errors: {
+    //     firstname: string | null;
+    //     lastname: string | null;
+    //     phone: string | null;
+    //     address: {
+    //         street: string | null;
+    //         number: string | null;
+    //         city: string | null;
+    //         provincia: string | null;
+    //     },
+    //     mail: string | null;
+    //     password: string | null;
+    //     confirmPassword: string | null;
+    // }
 }
-
-type actionType =
-    | { type: "change"; field: string; value: string }
-    | { type: "changeAddress"; field: keyof State['address']; value: string }
-    | { type: "submit" };
-
 const initialState: State = {
     firstname: "",
     lastname: "",
@@ -35,7 +43,28 @@ const initialState: State = {
     mail: "",
     password: "",
     confirmPassword: "",
+    // errors: {
+    //     firstname: "",
+    //     lastname: "",
+    //     phone: "",
+    //     address: {
+    //         street: "",
+    //         number: "",
+    //         city: "",
+    //         provincia: "",
+    //     },
+    //     mail: "",
+    //     password: "",
+    //     confirmPassword: "",
+    // }
 };
+
+type actionType =
+    | { type: "change"; field: string; value: string }
+    | { type: "catchError"; field: string; value: string }
+    | { type: "clearError" }
+    | { type: "changeAddress"; field: keyof State['address']; value: string }
+    | { type: "submit" };
 
 function reducer(state: State, action: actionType): State {
     switch (action.type) {
@@ -60,6 +89,8 @@ function reducer(state: State, action: actionType): State {
 }
 
 export default function SingUpForm() {
+
+
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
         dispatch({
             type: "change",
@@ -78,8 +109,8 @@ export default function SingUpForm() {
 
     const handleSubmit = (e: React.SyntheticEvent): void => {
         e.preventDefault();
-        dispatch({ type: "submit" });
-        console.log("Form Submitted", state);
+        console.log(state);
+
     }
 
     const [state, dispatch] = useReducer(reducer, initialState);
@@ -87,51 +118,59 @@ export default function SingUpForm() {
         <>
             <form onSubmit={handleSubmit} className="Form">
                 <div className="Fields">
-                    <h4>Inzia a fare affari, crea il tuo account</h4>
+                    <h4>Inizia a fare affari, crea il tuo account</h4>
                     <div className="Field">
                         <label>Nome*</label>
-                        <input type="text" name="firstname" placeholder="nome" value={state.firstname} onChange={handleChange}></input>
+                        <input type="text" name="firstname" placeholder="nome" value={state.firstname} onChange={handleChange} ></input>
                     </div>
                     <div className="Field">
                         <label>Cognome*</label>
-                        <input type="text" name="lastname" placeholder="cognome" value={state.lastname} onChange={handleChange}></input>
+                        <input type="text" name="lastname" placeholder="cognome" value={state.lastname} onChange={handleChange} ></input>
+                        <p className="Errors" >{ }</p>
                     </div>
                     <div className="Field">
                         <label>Telefono*</label>
-                        <input type="text" name="phone" placeholder="telefono" value={state.phone} onChange={handleChange}></input>
+                        <input type="text" name="phone" placeholder="telefono" value={state.phone} onChange={handleChange} ></input>
+                        <p className="Errors" >{ }</p>
                     </div>
                     <div className="Multiple">
                         <div className="Field" style={{ width: '80%' }}>
                             <label>Indirizzo*</label>
-                            <input type="text" name="street" placeholder="via" value={state.address.street} onChange={handleAddressChange}></input>
+                            <input type="text" name="street" placeholder="via" value={state.address.street} onChange={handleAddressChange} ></input>
+                            <p className="Errors" >{ }</p>
                         </div>
                         <div className="Field" style={{ width: '20%' }}>
                             <label>n*</label>
-                            <input type="text" name="number" placeholder="n." value={state.address.number} onChange={handleAddressChange}></input>
+                            <input type="text" name="number" placeholder="n." value={state.address.number} onChange={handleAddressChange} ></input>
+                            <p className="Errors" >{ }</p>
                         </div>
                     </div>
                     <div className="Multiple">
                         <div className="Field" style={{ width: '80%' }}>
                             <label>Città*</label>
-                            <input type="text" name="city" placeholder="città" value={state.address.city} onChange={handleAddressChange}></input>
+                            <input type="text" name="city" placeholder="città" value={state.address.city} onChange={handleAddressChange} ></input>
+                            <p className="Errors" >{ }</p>
                         </div>
                         <div className="Field" style={{ width: '20%' }}>
                             <label>Pv*</label>
-                            <input type="text" name="provincia" placeholder="Pv" value={state.address.provincia} onChange={handleAddressChange}></input>
+                            <input type="text" name="provincia" placeholder="Pv" value={state.address.provincia} onChange={handleAddressChange} ></input>
+                            <p className="Errors" >{ }</p>
                         </div>
                     </div>
                     <div className="Field">
                         <label>Email*</label>
-                        <input type="mail" name="mail" placeholder="mail" value={state.mail} onChange={handleChange}></input>
+                        <input type="mail" name="mail" placeholder="mail" value={state.mail} onChange={handleChange} ></input>
                     </div>
                     <div className="Field">
                         <label>Password*</label>
-                        <input type="password" name="password" placeholder="password" value={state.password} onChange={handleChange}></input>
+                        <input type="password" name="password" placeholder="password" value={state.password} onChange={handleChange} ></input>
                         <label className="Suggestion">la password deve essere di almeno 7 caratteri, 2 numeri e 1 carattere speciale</label>
+                        <p className="Errors" >{ }</p>
                     </div>
                     <div className="Field">
                         <label>Conferma password*</label>
                         <input type="password" name="confirmPassword" placeholder="conferma password" value={state.confirmPassword} onChange={handleChange}></input>
+                        <p className="Errors" >{ }</p>
                         <p style={{ marginBottom: '0' }}>* campo obbligatorio</p>
                     </div>
                 </div>
