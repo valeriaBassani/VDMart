7784bb7d49dd92780e7dde4a4e611e7507fd889a
ValeriaBassaniVDM <valeria.bassani@vidiemme.it>
2024-09-02 18:01:26 +0200

    adj.

--- src/pages/ActiveAdvDetails.tsx
+++ src/pages/ActiveAdvDetails.tsx
@@ -1,9 +1,74 @@
-import ActiveAdv from "../components/Organisms/ActiveAdv/ActiveAdv";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import Button from "../components/Atoms/Buttons/Buttons";
+import AdvImages from "../components/Molecules/AdvImages/AdvImages";
+import { DeleteAdv } from "../components/Molecules/DeleteAdv/DeleteAdv";
+import { AdvData, emptyAds, getActualAdv } from "../storesData/products";
 
 export default function AdvDetails() {
+    const [adv, setAdv] = useState<AdvData>(emptyAds)
+
+    useEffect(() => {
+        const fetchAds = async () => {
+            try {
+                const adv = await getActualAdv()
+                setAdv(adv);
+            } catch (error) {
+                console.error("Errore durante il recupero dell'annuncio", (error as Error).message);
+            }
+        };
+        fetchAds();
+    }, []);
+
+    const navigate = useNavigate();
+    const handleClick = () => {
+        navigate('/modifica-annuncio-attivo');
+    }
+
     return (
         <>
-            <ActiveAdv />
+            <div className="container-lg mb-5">
+                <h4>Dettagli annuncio attivo</h4>
+                <div className="main__section mt-2 p-4">
+                    <div className="row">
+                        <div className="col">
+                            <AdvImages adv={adv} />
+                        </div>
+                        <div className="col d-flex flex-column gap-5 mt-5">
+                            <div className="row gap-2">
+                                <div className="row">
+                                    <div className="col">
+                                        <h5 className="adv__category">Tecnologia</h5>
+                                    </div>
+                                </div>
+                                <h4>Ipad terza generazione nuovo</h4>
+                                <p className="adv__date">07/10/2023</p>
+                                <h3 className="adv__price">145,00€</h3>
+                                <div className="row">
+                                    <div className="col-auto ">
+                                        <div className="col d-flex gap-2" id="shipping">
+                                            <p className="adv__shipping">Spedizione disponibile: </p>
+                                            <p>costo: 24,90€</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="col d-flex gap-2 ">
+                                    <Button className="btn--primary" onClick={handleClick} wide={true}>Modifica annuncio</Button>
+                                    <DeleteAdv article="bici" />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col adv__description">
+                            <h4>Descrizione</h4>
+                            <p>{adv.description}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     )
 
--- src/pages/OtherUser.tsx
+++ src/pages/OtherUser.tsx
@@ -27,7 +27,6 @@ export default function OtherUser() {
                 <UserArea isActual={false} user={user} />
                 <OtherUserHistory user={user} />
             </div>
-
         </>
     )
 }
