4408281fb57d977bcf2488491c9bdc77ee5e3fab
ValeriaBassaniVDM <valeria.bassani@vidiemme.it>
2024-09-16 11:27:00 +0200

    errorSupport

--- src/components/Molecules/SupportForm/SupportForm.tsx
+++ src/components/Molecules/SupportForm/SupportForm.tsx
@@ -15,24 +15,57 @@ export default function SupportForm({ category }: Props) {
     const { t } = useTranslation();
     const [show, setShow] = useState(false);
 
+    const [errors, setErrors] = useState({
+        mail: "",
+        text: "",
+    })
+
+    const validateForm = async (mail: string, text: string): Promise<boolean> => { //in store
+        let errors = false
+        setErrors({
+            mail: "",
+            text: "",
+        })
+        if (mail === '') {
+            errors = true
+            setErrors(prevErrors => ({
+                ...prevErrors,
+                mail: 'Campo obbligatorio'
+            }));
+        }
+        if (text === '') {
+            errors = true
+            setErrors(prevErrors => ({
+                ...prevErrors,
+                text: 'Campo obbligatorio'
+            }));
+        }
+        return errors
+    }
+
     const handleClick = useCallback(() => {
         setShow(!show)
     }, [show])
 
-    const handleSubmit = useCallback((e: React.FormEvent<HTMLFormElement>): void => {
+    const handleSubmit = useCallback(async (e: React.FormEvent<HTMLFormElement>): Promise<void> => {
         e.preventDefault();
         const formData = new FormData(e.currentTarget);
         const email = formData.get('email') as string;
         const text = formData.get('text') as string;
-        console.log(email, text);
-    }, [])
+        const error = validateForm(email, text)
+
+        if (!(await error)) {
+            setShow(!show)
+        }
+
+    }, [show])
 
     return (
         <>
             <form onSubmit={handleSubmit} className="form">
                 <div className="container-md  main__section">
                     <h4>{t("supportForm.form.title")}</h4>
-                    <InputField label="Email" type="email" name="email" placeholder="email" required={true} ></InputField>
+                    <InputField label="Email" type="email" name="email" error={errors.mail} placeholder="email" required={true} ></InputField>
                     <div className="create__section">
                         <div className="row">
                             <div className="col d-flex gap-2 py-2">
@@ -40,9 +73,9 @@ export default function SupportForm({ category }: Props) {
                                 <p> {category} </p>
                             </div>
                         </div>
-                        <TextArea label={t("supportForm.form.message")} name="text" maxLength={300} required={true} />
+                        <TextArea label={t("supportForm.form.message")} name="text" error={errors.text} maxLength={300} required={true} />
                     </div>
-                    <Submit label={t("supportForm.form.submit")} className='btn--primary' onClick={handleClick} />
+                    <Submit label={t("supportForm.form.submit")} className='btn--primary' />
                 </div>
             </form >
 
