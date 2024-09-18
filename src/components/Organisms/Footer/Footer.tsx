2f219995b4338f1129a8de2f8f17e22e29b54d09
ValeriaBassaniVDM <bassanivaleria01@gmail.com>
2024-09-18 16:41:16 +0200

    translation

--- src/components/Organisms/Footer/Footer.tsx
+++ src/components/Organisms/Footer/Footer.tsx
@@ -2,8 +2,10 @@ import { Link } from "react-router-dom";
 import './Footer.css';
 import Icon from "../../Atoms/Icon";
 import logo from './logo.svg';
+import { useTranslation } from "react-i18next";
 
 export default function Footer() {
+    const { t } = useTranslation();
     return (
         <div className="footer">
             <div className="row">
@@ -11,11 +13,11 @@ export default function Footer() {
                     <Link to={`/`}><Icon url={logo} alt="home" /></Link>
                 </div>
                 <div className="col-4 d-flex flex-column gap-4">
-                    <Link to={`/assistenza`} className="navbar__link">Assistenza</Link>
+                    <Link to={`/assistenza`} className="navbar__link">{t('supportLink.link-2')}</Link>
                     <Link to={`/`} className="navbar__link">Privacy</Link>
-                    <Link to={`/vendi`} className="navbar__link">Crea un annuncio</Link>
-                    <Link to={`/`} className="navbar__link">Mappa del sito</Link>
-                    <Link to={`/login`} className="navbar__link">Accedi</Link>
+                    <Link to={`/vendi`} className="navbar__link">{t('navbar.create')}</Link>
+                    <Link to={`/`} className="navbar__link">{t('navbar.map-site')}</Link>
+                    <Link to={`/login`} className="navbar__link">{t('navbar.logIn')}</Link>
                 </div>
                 <div className="col-4 text-end">
                     <Link to={`/`} className="navbar__link">Social</Link>
