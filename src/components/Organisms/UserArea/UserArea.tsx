f3fd3b525b7abeff6d41a99262c7e6cdba44610e
ValeriaBassaniVDM <bassanivaleria01@gmail.com>
2024-09-19 17:50:33 +0200

    translations

--- src/components/Organisms/UserArea/UserArea.tsx
+++ src/components/Organisms/UserArea/UserArea.tsx
@@ -3,12 +3,14 @@ import ReviewPreview from "../../Molecules/ReviewPreview/ReviewPreview";
 import { getRatingVote, User } from "../../../storesData/account";
 import { useEffect, useState } from "react";
 import "./UserArea.css"
+import { useTranslation } from "react-i18next";
 
 type Props = {
     user: User
     isActual: boolean
 }
 export default function UserArea({ user, isActual }: Props) {
+    const { t } = useTranslation();
     const [totRate, setTotRate] = useState(0)
     useEffect(() => {
         const fetchAds = async () => {
@@ -26,12 +28,12 @@ export default function UserArea({ user, isActual }: Props) {
                 <div className="row">
                     <div className="col">
                         <div className="row gap-2">
-                            <h4>Recensioni da altri utenti</h4>
+                            <h4>{t('reviews.title')}</h4>
                             <div className="col d-flex gap-2 align-items-end">
-                                <p>Punteggio complessivo: </p><h3 style={{ color: "var(--primary)" }}>{totRate}</h3>
+                                <p>{t('reviews.overallScore')} </p><h3 style={{ color: "var(--primary)" }}>{totRate}</h3>
                             </div>
                             <div className="col-auto d-flex justify-content-end align-items-end">
-                                <p>Tot:{user.review.length} recensioni</p>
+                                <p>Tot: {user.review.length} {t('reviews.totalReviews')}</p>
                             </div>
                         </div>
                     </div>
@@ -43,19 +45,16 @@ export default function UserArea({ user, isActual }: Props) {
                                 <ReviewPreview rev={rev} />
                             ))
                         ) : (
-                            <p>Nessuna recensione</p>
+                            <p>{t('reviews.noReviews')}</p>
                         )}
                     </div>
                 </div>
                 {user.review.length > 2 && (<div className="row text-end">
                     <div className="col">
-                        {isActual ? (<Link to={`/tutte-le-recensioni`} className="link">Vedi tutte</Link>) : (<Link to={`/tutte-le-recensioni-utente`} className="link">Vedi tutte</Link>)}
+                        {isActual ? (<Link to={`/tutte-le-recensioni`} className="link">{t('reviews.seeAllReviews')}</Link>) : (<Link to={`/tutte-le-recensioni-utente`} className="link">{t('reviews.seeAllReviews')}</Link>)}
                     </div>
                 </div>)}
-
             </div >
-
-
         </>
     )
 }
